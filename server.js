251ef925d75f1730d00e700a640d9de9d14212c3
Jagan Pradhan <jaganp515@gmail.com>
2026-01-23 15:32:51 +0530

    Day 3

--- server.js
+++ server.js
@@ -29,6 +29,17 @@ app.post('/register', (request, response) => {
         password // In real applications, always hash passwords
     };
 
+   
+    // Check if email already exists
+    const existingUser = users.find(userEmail => userEmail.email === email);
+    if (existingUser) {
+        return response.status(409).json({
+            message: `Email already registered: ${email}`
+        });
+    }
+
+    
+
     // Store user
     users.push(newUser);
 
@@ -41,4 +52,36 @@ app.post('/register', (request, response) => {
 
 app.listen(5001, () => {
     console.log('Server is running on port 5001');
+});
+
+/**
+ * Create a post api with path /login which wtakes in email and password
+ * from body and check if user with same email and password exist in the 
+ * users array .if yes return 200 reponse ,otherwise 400 response.
+ */
+
+app.post('/login', (request, response) => {
+    const { email, password } = request.body;
+
+    // Validation
+    if (!email || !password) {
+        return response.status(400).json({
+            message: 'Email and password are required'
+        });
+    }
+
+    // Check if user exists
+    const validUser = users.find(
+        user => user.email === email && user.password === password
+    );
+
+    if (validUser) {
+        return response.status(200).json({
+            message: 'User login successful'
+        });
+    } else {
+        return response.status(400).json({
+            message: 'Invalid email or password'
+        });
+    }
 });
